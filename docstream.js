c6f0f78e72e1424d7cbad129f61af50ff8e8dc68
jhejderup <joseph.hejderup@gmail.com>
2017-01-23 15:20:57 +0100

    ES6-ified it and perform the "id"-test

--- docstream.js
+++ docstream.js
@@ -1,17 +1,10 @@
-// libraries
-var liner = require("./lib/liner.js"),
-  parser = require('./lib/parser.js');
+const liner = require('./lib/liner.js')
+const parser = require('./lib/parser.js')
 
-// command-line parameters
-var rs = process.stdin;
-var os = process.stdout;
+const rs = process.stdin
+const os = process.stdout
 
 // pipe the input to the output, via transformation functions
-rs.pipe(liner)                // turn each line of the file into a single string
-  .pipe(parser)               // attempt to parse each line
-  .pipe(os);                  // write the output to stdout
-
-
-
-  
-   
+rs.pipe(liner) // turn each line of the file into a single string
+  .pipe(parser) // attempt to parse each line
+  .pipe(os) // write the output to stdout

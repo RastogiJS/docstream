879277ad33b1e422ac9738493ebedb8c26f42f53
jhejderup <joseph.hejderup@gmail.com>
2017-01-24 11:51:02 +0100

    added some comments

--- docstream.js
+++ docstream.js
@@ -8,5 +8,5 @@ const os = process.stdout
 // pipe the input to the output, via transformation functions
 rs.pipe(liner) // turn each line of the file into a single string
   .pipe(parser) // attempt to parse each line
-  .pipe(resolver) // write the output to stdout
-  .pipe(os)
+  .pipe(resolver) //resolve all dependencies for each version of a document
+  .pipe(os) // write the output to stdout

965ca92aa0f5373f60eb6a0ba87416d64e3bb934
jhejderup <joseph.hejderup@gmail.com>
2017-01-25 11:03:43 +0100

    load balancing with redis

--- docstream.js
+++ docstream.js
@@ -1,6 +1,7 @@
-const liner = require('./lib/liner.js')
-const parser = require('./lib/parser.js')
+const liner = require('./lib/liner')
+const parser = require('./lib/parser')
 const resolver = require('./lib/resolver')
+const redis = require('./lib/redis')
 
 const rs = process.stdin
 const os = process.stdout
@@ -8,5 +9,5 @@ const os = process.stdout
 // pipe the input to the output, via transformation functions
 rs.pipe(liner) // turn each line of the file into a single string
   .pipe(parser) // attempt to parse each line
- // .pipe(resolver) //resolve all dependencies for each version of a document
+  .pipe(redis) //resolve all dependencies for each version of a document
   .pipe(os) // write the output to stdout

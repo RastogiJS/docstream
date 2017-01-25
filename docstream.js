7e365dc97ab13c624bf46939742c55f3e917071f
jhejderup <joseph.hejderup@gmail.com>
2017-01-25 12:40:34 +0100

    removing redis

--- docstream.js
+++ docstream.js
@@ -1,7 +1,6 @@
 const liner = require('./lib/liner')
 const parser = require('./lib/parser')
 const resolver = require('./lib/resolver')
-const redis = require('./lib/redis')
 
 const rs = process.stdin
 const os = process.stdout
@@ -9,5 +8,6 @@ const os = process.stdout
 // pipe the input to the output, via transformation functions
 rs.pipe(liner) // turn each line of the file into a single string
   .pipe(parser) // attempt to parse each line
-  .pipe(redis) //resolve all dependencies for each version of a document
+  .pipe(resolver) //resolve all dependencies for each version of a document
   .pipe(os) // write the output to stdout
+
